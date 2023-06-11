a3d0aae19561f1791671c45ff1aef4be6dc86655
framewirkdev <115645221+framewirkdev@users.noreply.github.com>
2023-06-12 01:04:44 +0530

    Add files via upload

---
+++ voluntable-registry/scripts/docker-entrypoint.sh
@@ -0,0 +1,1 @@
+#!/bin/shif [ ! -f .initialized ]; then    echo "Initializing container"    sh /scripts/font-config.sh    touch .initializedelse    echo "Already initialized"fiecho "Starting the server"npm start
